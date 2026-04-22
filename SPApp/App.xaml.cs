27c86a7b2cfbc351743148b39f962dd8c7b2316a
danielted23 <daniel2003@outlook.dk>
2026-04-22 10:13:56 +0200

    Add project files.

---
+++ SPApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SPApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
